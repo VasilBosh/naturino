0733c5dd48faf25eb91aea0c84cb949fc4843152
VasilBosh <catch222@abv.bg>
2026-09-12 10:35:27 +0300

    add new comments 12.09.2026

--- src/sections/CheckoutSocialProof.tsx
+++ src/sections/CheckoutSocialProof.tsx
@@ -1,7 +1,7 @@
 import { MessageCircle } from 'lucide-react';
 
 const reviews = Array.from(
-  { length: 15 },
+  { length: 22 },
   (_, index) => `/social/${index + 1}.jpg`
 );
 
--- src/sections/ReviewsSlider.tsx
+++ src/sections/ReviewsSlider.tsx
@@ -11,6 +11,7 @@ const reviews = [
   '/social/8.jpg',
   '/social/9.jpg',
   '/social/15.jpg',
+  '/social/16.jpg','/social/17.jpg','/social/18.jpg','/social/19.jpg','/social/20.jpg','/social/21.jpg','/social/22.jpg',
 ];
 
 export function ReviewsSlider() {
